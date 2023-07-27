7eede846fe83c386ed4748672798b6e835e207c2
zakariariaria <zakaria.bouhemhem@mail.mcgill.ca>
2023-07-27 00:21:45 -0400

    fixed size issues for containers

--- src/Forms/Login.js
+++ src/Forms/Login.js
@@ -8,34 +8,27 @@ function Login() {
         e.target.value = "";
     }
 
-    const navigate = useNavigate();
-
-    function handleClick(){
-        navigate("/test")
-    }
     
     return (
-        <div id="container">
-            <div id="left_container">
-                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-            </div>
-            <div id="right_container">
-                <h1>Register</h1>
-                <label> Don't have an account ?</label>
-                <a onClick={handleClick} href="">Create one, it takes less than a minute</a>
-                
-                <input type="text" defaultValue="Email" onClick={resetInput} ></input>
-                <input type="password" defaultValue="Password" onClick={resetInput}></input>
-                <button>Log In</button>
-                <label>Log in using social media instead</label>
-                <div id="social_media_container">
-                    <button>twitter</button>
-                    <button>facebook</button>
-                    <button>google</button>
+        <body>
+            <div id="wrapper">
+                <div id="left_wrapper">
+
+                    <label>Dapper</label>
+                    <h1>Sign In</h1>
+                    <input type="text" defaultValue="Email" onClick={resetInput} ></input>
+                    <input type="password" defaultValue="Password" onClick={resetInput}></input>
+                    <button>Login</button>
+                    <a href="test">CREATE NEW ACCOUNT</a>
+                    
                 </div>
-            </div>
+                <div id="right_wrapper">
+                </div>
+                
             
-        </div>
+         </div>
+        </body>
+        
     )
 }
 
