2b2cdd1f32413fe870d2712ac1ca01c73645217e
zakariariaria <zakaria.bouhemhem@mail.mcgill.ca>
2023-07-28 23:37:52 -0400

    finished draft login

--- src/Forms/Login.js
+++ src/Forms/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate} from 'react-router-dom';
 import '../Styles/Login.css'
+import { BsArrowRight } from 'react-icons/bs';
 
 function Login() {
 
@@ -14,15 +15,37 @@ function Login() {
             <div id="wrapper">
                 <div id="left_wrapper">
 
-                    <label>Dapper</label>
-                    <h1>Sign In</h1>
-                    <input type="text" defaultValue="Email" onClick={resetInput} ></input>
-                    <input type="password" defaultValue="Password" onClick={resetInput}></input>
-                    <button>Login</button>
-                    <a href="test">CREATE NEW ACCOUNT</a>
-                    
+                    <div id="logo">
+                        <label>Dapper</label>
+                    </div>
+
+                    <div id="info_wrapper">
+                        <label id="subtitle">START YOUR UNIQUE CLOTHING EXPERIENCE</label>
+                        <h1 id="title">Login to your account</h1>
+                        <div className="input_wrappers">
+                            <label>E-Mail</label>
+                            <input type="text" defaultValue="Email" onClick={resetInput} className="text_inputs"></input>
+                        </div>
+                        <div className="input_wrappers">
+                            <label>Password</label>
+                            <input type="password" defaultValue="Password" onClick={resetInput} className="text_inputs"></input>
+                        </div>
+                        
+                        <div id="buttons_wrapper">
+                            <button id="signup_button">Register</button>
+                            <button id="login_button">Login</button>
+                        </div>
+                        <div id="account_wrapper">
+                            <div id="checkbox_wrapper">
+                                <input type="checkbox"></input>
+                                <label>Remember me</label>
+                            </div>
+                            <a href="#">Forgot password?</a>
+                        </div>
+                    </div>      
                 </div>
                 <div id="right_wrapper">
+                
                 </div>
                 
             
