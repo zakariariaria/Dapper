81e01ccbd4d39d64676cfeca54e9fe64908d0fbe
Amine <amine@MacBook-Pro-de-Amine.local>
2023-09-14 15:00:18 -0400

    deleted node mudles and added assets

--- Frontend/src/Forms/Home.js
+++ Frontend/src/Forms/Home.js
@@ -3,6 +3,12 @@ import "../Styles/Home.css";
 import placeholder from "../Images/placeholder-image.png";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
+import hautecouture from "../Images/hauteCouture_clothing.jpg";
+import vintagecouture from "../Images/vintage_clothing.jpg";
+import formalcouture from "../Images/formalWear_couture.jpg";
+import casualcouture from "../Images/casual_clothing.jpg";
+import { motion } from 'framer-motion';
+
 
 function Home() {
   return (
@@ -19,63 +25,100 @@ function Home() {
 
         <div id="wrapper_work_home">
           <div className="wrapper_how_it_works">
-          <h2>How it works</h2>
-          <ul id="work_list">
-            <li>Choose your clothes</li>
-            <li>Delivered safely to your door</li>
-          </ul>
-          <div className="wrapper_images_home">
-            <div className="wrapper_choices">
-              <img className="placeholder"></img>
-              <h3>Choose your clothes</h3>
-              <label>Curated outfits</label>
+            <h2>How it works</h2>
+            <ul id="work_list">
+              <li>Choose your clothes</li>
+              <li>Delivered safely to your door</li>
+            </ul>
+            <div className="wrapper_images_home">
+              <div className="wrapper_choices">
+                <img className="placeholder"></img>
+                <h3>Choose your clothes</h3>
+                <label>Curated outfits</label>
+              </div>
+              <div className="wrapper_choices">
+                <img className="placeholder"></img>
+                <h3>Create the perfect box</h3>
+                <label>Curated outfits</label>
+              </div>
+              <div className="wrapper_choices">
+                <img className="placeholder"></img>
+                <h3>Get convenient deliveries</h3>
+                <label>Curated outfits</label>
+              </div>
             </div>
-            <div className="wrapper_choices">
-              <img  className="placeholder"></img>
-              <h3>Create the perfect box</h3>
-              <label>Curated outfits</label>
-            </div>
-            <div className="wrapper_choices">
-              <img  className="placeholder"></img>
-              <h3>Get convenient deliveries</h3>
-              <label>
-              Curated outfits
-              </label>
-            </div>
-            </div>
-          <button>Learn more</button>
+            <button>Learn more</button>
           </div>
+          {/* --------------------------- */}
           <hr className="line_home"></hr>
-          <h1>Your box, your way</h1>
-          <label>
-            Flexible options to make you look dapper and customize your box
-          </label>
-          <div className="wrapper_images_home">
-            <div className="wrapper_options_home">
-              <h3>Casual wear</h3>
-              <label>Occasional, spontaneous and suited for everyday use</label>
+          <div className="wrapper_how_it_works">
+            <motion.h2 whileHover={{scale: 1.1}}>Your box, your way</motion.h2>
+            <motion.label>
+              Flexible options to make you look dapper and customize your box
+            </motion.label>
+            <div className="wrapper_images_home">
+              <div className="wrapper_choices">
+                <img
+                  className="image-container"
+                  style={{
+                    height: "400px",
+                    width: "300px",
+                    objectFit: "cover",
+                  }}
+                  src={casualcouture}
+                ></img>
+                <motion.h3>Casual wear</motion.h3>
+                <motion.label>
+                  Occasional, spontaneous and suited for everyday use
+                </motion.label>
+              </div>
+              <div className="wrapper_choices">
+                <img
+                  className="image-container"
+                  style={{
+                    height: "400px",
+                    width: "300px",
+                    objectFit: "cover",
+                  }}
+                  src={vintagecouture}
+                ></img>{" "}
+                <motion.h3>Vintage clothing</motion.h3>
+                <motion.label>Reflect the styles and trends of past eras</motion.label>
+              </div>
+              <div className="wrapper_choices">
+                <img
+                  className="image-container"
+                  style={{
+                    height: "400px",
+                    width: "300px",
+                    objectFit: "cover",
+                  }}
+                  src={hautecouture}
+                ></img>{" "}
+                <motion.h3>Haute couture</motion.h3>
+                <motion.label>For those seeking high-end fashion design</motion.label>
+              </div>
+              <div className="wrapper_choices">
+                <img
+                  className="image-container"
+                  style={{
+                    height: "400px",
+                    width: "300px",
+                    objectFit: "cover",
+                  }}
+                  src={formalcouture}
+                ></img>{" "}
+       <motion.h3 whileHover={{ scale: 1.1 }}>Formal wears</motion.h3>
+      <motion.label whileHover={{ scale: 1.1 }}>
+        For the most formal of occasions
+      </motion.label>
+              </div>
             </div>
-            <div className="wrapper_options_home">
-              <h3>Vintage clothing</h3>
-              <label>Reflect the styles and trends of past eras</label>
-            </div>
-            <div className="wrapper_options_home">
-              <h3>Haute couture</h3>
-              <label>For those seeking high-end fashion design</label>
-            </div>
-            <div className="wrapper_options_home">
-              <h3>Formal wear</h3>
-              <label>For the most formal of occasions</label>
-            </div>
-          </div>
-          <button>See our styles</button>
-          <div id="wrapper2_home">
-            <div id="wrapper2_title">
-              <h1>Get started</h1>
-              <button className="plans_button_home">View our plans</button>
-            </div>
+            <button>See our styles</button>
           </div>
         </div>
+        <hr className="line_home"></hr>
+
         <Footer></Footer>
       </div>
     </body>
