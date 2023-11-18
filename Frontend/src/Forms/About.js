a7d2ff78ef602d2901032323068d08d375704b17
Zakaria Bouhemhem <zak@MacBook-Air-de-Zakaria.local>
2023-11-18 17:22:09 -0500

    finished about

--- Frontend/src/Forms/About.js
+++ Frontend/src/Forms/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Styles/About.css';
 import NavBar from '../Components/Navbar.js';
+import Footer from '../Components/Footer.js'
 
 function About() {
 
@@ -15,35 +16,54 @@ function About() {
         padding: '10px 20px',
         cursor: 'pointer',
     }
+
+    const row = {
+        display: 'flex',
+        flexDirection: 'row',
+        color:'white',
+        justifyContent:'space-between',
+        alignItems:'center',
+        width:'96%',
+        height:'500px',
+        gap:'1%'
+    }
+
+    const boxes = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:'center',
+        height:'300px'
+    }
     
   return (
     <body>
         <NavBar></NavBar>
-        <div className="container-xxl border" style={{display: 'flex',flexDirection: 'column',alignItems:'center', marginTop:'2%', colorBackground:'#0e1111',color:'white'}}>
+        <div className="container-fixed border pt-3" id="top-container">
             <h1>Step 1: Sign Up and Personalize</h1>
             <p> Sign up for Dapper and fill out a profile detailing your fashion preferences, sizes, preferred colors, and subscription level. </p>
             <button class="btn btn-outline-primary" style={btn}>Sign Up</button>
         </div>
-        <div className="container-xl  pt-3" style={{display: 'flex',flexDirection: 'row',color:'white',justifyContent:'space-between',width:'100%',gap:'5%'}} >
-        <div className="container border pt-3" style={{display: 'flex',flexDirection: 'column',alignItems:'center'}}>
+        <div className="container-fixed  pt-3" style={row} >
+        <div className="container border pt-3" style={boxes}>
             <h1>Step 2: Stylist Curates Outfits</h1>
             <p>Each month, our expert fashion stylists curate outfits tailored to the user's individual style and preferences.</p>
         </div>
-        <div className="container border pt-3" style={{display: 'flex',flexDirection: 'column',alignItems:'center'}}>
+        <div className="container border pt-3" style={boxes}>
             <h1>Step 3: Home Delivery</h1>
             <p>These personalized outfits are delivered directly to the user's residence every month, offering the convenience and excitement of new styles right at their doorstep.</p>
         </div>
         </div>
-        <div className="container-xl  pt-3" style={{display: 'flex',flexDirection: 'row',color:'white'}} >
-        <div className="container-lg border pt-3" style={{display: 'flex',flexDirection: 'column',alignItems:'center'}}>
+        <div className="container-fixed pt-3" style={row} >
+        <div className="container border pt-3" style={boxes}>
             <h1>Step 4: Try and Decide</h1>
             <p>Users have the opportunity to try out various outfits. This flexibility allows you to experiment with different styles without the pressure of a permanent purchase.</p>
         </div>
-        <div className="container-lg border pt-3" style={{display: 'flex',flexDirection: 'column',alignItems:'center'}}>
+        <div className="container border pt-3" style={boxes}>
             <h1>Step 5: Keep What You Love</h1>
             <p>If you fall in love with an item, you have the option to purchase it. Otherwise, the outfits are picked up by our delivery service the next month.</p>
         </div>
         </div>
+        <Footer></Footer>
     </body>
     
   );
