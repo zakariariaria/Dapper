986df2003c1f4f8cf489672cf8a73e11072bf7d3
Amine <amine@MacBook-Pro-de-Amine.local>
2023-11-20 12:57:52 -0500

    edited navbar

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion"; // Step 1: Import motion
 import "../Styles/NavBar.css";
 import logoSVG from "../Images/LogoNavb.png";
+import videoMP4 from "../animation/logo-black-unscreen.gif"; // Import the video file
+
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -20,32 +22,24 @@ const Navbar = () => {
   // Step 2: Define the animation variants for the hover effect
   const hoverVariant = {
     hover: {
-      scale: 1.1,
+      color: "white",
       transition: {
-        duration: 0.3
-      }
+        duration: 0.3,
+      },
     },
     initial: {
-      scale: 1
-    }
+      color: "#c5c2c2",
+    },
   };
 
   return (
     <div id="navbar_home">
-   <div className="logo_section">
-
-        <motion.img
-          src={logoSVG}
-          alt="Logo"
-          initial="initial"
-          whileHover="hover"
-          style={{width:"60px", cursor:"pointer"}}
-          onClick={logoClick}
-          
-        />
-      </div>
       <div className="navlinks_section">
-        <motion.a 
+      {/* <div className="logo_section">
+      <img src={videoMP4} alt="Logo" className="logo-image" />
+</div> */}
+
+        <motion.a
           href="/about"
           variants={hoverVariant}
           initial="initial"
@@ -53,7 +47,7 @@ const Navbar = () => {
         >
           How it works
         </motion.a>
-        <motion.a 
+        <motion.a
           href="/about"
           variants={hoverVariant}
           initial="initial"
@@ -61,7 +55,7 @@ const Navbar = () => {
         >
           What we do
         </motion.a>
-        <motion.a 
+        <motion.a
           href="/about"
           variants={hoverVariant}
           initial="initial"
@@ -69,11 +63,29 @@ const Navbar = () => {
         >
           Contact us
         </motion.a>
+        <motion.div
+          variants={hoverVariant}
+          initial="initial"
+          whileHover="hover"
+          onClick={navLogin}
+          style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-person-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+          </svg>
+        </motion.div>
       </div>
-      <div className="buttons_section">
+      {/* <div className="buttons_section">
         <button onClick={navLogin}>Log In</button>
         <button onClick={navSignup}>Sign Up</button>
-      </div>
+      </div> */}
     </div>
   );
 };
--- Frontend/src/Components/ProfilePageElements.js
+++ Frontend/src/Components/ProfilePageElements.js
@@ -42,7 +42,13 @@ function ProfilePageElements({
                 className="rounded-top text-white d-flex flex-row"
                 style={{ backgroundColor: "#0e1111", height: "200px" }}
               >
-                <div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
                   <MDBCardImage
                     src={profileImageSrc}
                     alt="Generic placeholder image"
@@ -56,33 +62,40 @@ function ProfilePageElements({
                       objectFit: "cover",
                     }}
                   />
-                    <div className="ms-3" style={{ display:"flex",flexDirection:"column"}}>
-                <MDBTypography tag="h5">{name}</MDBTypography>
-                  <MDBCardText>{location}</MDBCardText>
-                </div>
-                <div style={{display:"flex", flexDirection:"row"}}>
-                  <div>
-                    <MDBCardText className="mb-1 h5">{photosCount}</MDBCardText>
-                    <MDBCardText className="small  mb-0">Photos</MDBCardText>
+                  <div
+                    className="ms-3"
+                    style={{ display: "flex", flexDirection: "column" }}
+                  >
+                    <MDBTypography tag="h5">{name}</MDBTypography>
+                    <MDBCardText>{location}</MDBCardText>
                   </div>
-                  <div className="px-3">
-                    <MDBCardText className="mb-1 h5">
-                      {followersCount}
-                    </MDBCardText>
-                    <MDBCardText className="small  mb-0">Followers</MDBCardText>
-                  </div>
-                  <div>
-                    <MDBCardText className="mb-1 h5">
-                      {followingCount}
-                    </MDBCardText>
-                    <MDBCardText className="small  mb-0">Following</MDBCardText>
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <div>
+                      <MDBCardText className="mb-1 h5">
+                        {photosCount}
+                      </MDBCardText>
+                      <MDBCardText className="small  mb-0">Photos</MDBCardText>
+                    </div>
+                    <div className="px-3">
+                      <MDBCardText className="mb-1 h5">
+                        {followersCount}
+                      </MDBCardText>
+                      <MDBCardText className="small  mb-0">
+                        Followers
+                      </MDBCardText>
+                    </div>
+                    <div>
+                      <MDBCardText className="mb-1 h5">
+                        {followingCount}
+                      </MDBCardText>
+                      <MDBCardText className="small  mb-0">
+                        Following
+                      </MDBCardText>
+                    </div>
                   </div>
                 </div>
-                
-                </div>
-              
               </div>
-             
+
               <MDBCardBody className="text-white p-4">
                 <div className="mb-5">
                   <p className="lead fw-normal mb-1">About</p>
@@ -94,9 +107,11 @@ function ProfilePageElements({
                     ))}
                   </div>
                 </div>
-                <div style={{ display:"flex", justifyContent:"flex-end"}} >
-                  <button onClick={toggleShowAllPhotos} style={{borderRadius:"18px", padding:"0rem 0.5rem "}}>
-                    
+                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                  <button
+                    onClick={toggleShowAllPhotos}
+                    style={{ borderRadius: "18px", padding: "0rem 0.5rem " }}
+                  >
                     {showAllPhotos ? "Show Less" : "Show More"}
                   </button>
                 </div>
