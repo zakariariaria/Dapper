1020a87d07cd14eba5e6230da0595c3b3c9c7573
Zakaria Bouhemhem <zak@MacBook-Air-de-Zakaria.local>
2023-11-29 17:14:38 -0500

    fixed dropdown from account

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -5,6 +5,8 @@ import "../Styles/NavBar.css";
 import gif from "../animation/dapper_logo2.gif"; // Import the video file
 
 const Navbar = () => {
+
+  const [isDropdownHovered, setIsDropdownHovered] = useState(false);
   const navigate = useNavigate();
   const [activeUser, setActiveUser] = useState(null);
   const [showDropdown, setShowDropdown] = useState(false);
@@ -99,8 +101,14 @@ const Navbar = () => {
           {activeUser ? (
             <div
               className="parent-hover"
-              onMouseEnter={() => setShowDropdown(true)}
-              onMouseLeave={() => setShowDropdown(false)}
+              onMouseEnter={() => {
+                setShowDropdown(true);
+                setIsDropdownHovered(true);
+              }}
+              onMouseLeave={() => {
+                setShowDropdown(false);
+                setIsDropdownHovered(false);
+              }}
               style={{
                 display: "flex",
                 alignItems: "center",
@@ -122,13 +130,10 @@ const Navbar = () => {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-arrow-down-short"
+                className={`bi bi-arrow-down-short arrow-icon ${isDropdownHovered ? 'arrow-rotated' : ''}`}
                 viewBox="0 0 16 16"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4"
-                />
+                <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4" />
               </svg>
               {showDropdown && (
                 <div
